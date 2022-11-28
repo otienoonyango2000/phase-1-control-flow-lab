a46ac33aa9a7d554e7ab75bc1bba4e428debc28a
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-11-28 14:54:30 +0300

    control flow lab completed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  //let ride = 4000;
+  let charges = 30;
+  if (distance <= 400){
+    return 'This one is on me!'
+  } else if(distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if(distance > 2500){
+    return 'No can do.'
+  }
+};
+
+scuberGreetingForFeet();
+
+function ternaryCheckCity(destination){
+      if (destination !== 'NYC'){
+        return "No go."
+      } else {
+        return "Ok, sounds good."
+      }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  } else if(tip == "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
+  
 }
